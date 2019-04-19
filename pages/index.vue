5018508ddf22af03b5ef069f5e60506147ad607b
puterakahfi <puterakahfi@gmail.com>
2019-04-19 16:53:50 +0700

    update index

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,93 @@
 <template>
-  <div class="hero-mini bg">
-    <div class="container">
-      <div class="row align-items-center">
-        <div class="col-7">
-          <h1>REVIEW PLATFROM INVESTASI</h1>
-          <p
-            class="lead"
-          >Refview berbagai platform investasi berdasarkan pengalaman dari para lender dari group whatsapp, refview di aplikasi, blog, etc</p>
-          <div class="cta">
-            <b>
+  <div class="container mt-5">
+    <div class="row align-items-center">
+      <div class="col-7">
+        <h1>REVIEW PLATFROM INVESTASI</h1>
+        <p
+          class="lead"
+        >Refview berbagai platform investasi berdasarkan pengalaman dari para lender dari group whatsapp, refview di aplikasi, blog, etc</p>
+        <div class="cta">
+          <b>
             <a
               class="btn btn-lg btn-primary btn-icon icon-right shadowless"
               href="https://goo.gl/forms/72OsZ6lTMde84YkO2"
               target="_blank"
             >
-              <fa :icon="['fab', 'whatsapp']" class="fa-lg"/> Daftar di group wa agri invest
+              <fa :icon="['fab', 'whatsapp']" class="fa-lg"/>&nbsp;Daftar di group wa agri invest
             </a>
-            </b>
-          </div>
+          </b>
         </div>
         <div class="col-5"></div>
       </div>
+    </div>
+
+    <div class="row mt-5 pt-5">
+      <div class="col-md-12">
+        <div class="row">
+          <div class="col">
+
+            
+            <div class="card">
+              <div class="card-header">
+                <h4>Crowde</h4>
+              </div>
+              <div class="card-body">This is some text within a card body.</div>
+              <div class="card-footer">Footer Card</div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card">
+              <div class="card-header">
+                <h4>IGrow</h4>
+              </div>
+              <div class="card-body">This is some text within a card body.</div>
+              <div class="card-footer">Footer Card</div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card">
+              <div class="card-header">
+                <h4>Growpal</h4>
+              </div>
+              <div class="card-body">This is some text within a card body.</div>
+              <div class="card-footer">Footer Card</div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+         <div class="col-md-12">
+        <div class="row">
+          <div class="col">
+            <div class="card">
+              <div class="card-header">
+                <h4>Tanijoy</h4>
+              </div>
+              <div class="card-body">This is some text within a card body.</div>
+              <div class="card-footer">Footer Card</div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card">
+              <div class="card-header">
+                <h4>Ammana</h4>
+              </div>
+              <div class="card-body">This is some text within a card body.</div>
+              <div class="card-footer">Footer Card</div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card">
+              <div class="card-header">
+                <h4>Tanihub</h4>
+              </div>
+              <div class="card-body">This is some text within a card body.</div>
+              <div class="card-footer">Footer Card</div>
+            </div>
+          </div>
+        </div>
+      </div>
+
     </div>
   </div>
 </template>
@@ -36,10 +103,9 @@ export default {
 </script>
 
 <style sccoped>
-
 .btn.btn-lg {
-    padding: .55rem 1.5rem;
-    font-size: 14px;
+  padding: 0.55rem 1.5rem;
+  font-size: 14px;
 }
 
 h1 {
